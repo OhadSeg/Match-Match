b999ff4ee2746171d50f66599646a39c3f9672a1
erane <eranyo@mta.ac.il>
2023-09-14 22:47:34 +0300

    css changes

--- Server/configs/database.js
+++ Server/configs/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_CONNECT_URL).then(() => {
-  console.log("Mongoose server has started");
-});
-
-// mongoose.connect("mongodb://0.0.0.0:27017/usersDB").then(() => {
-//   console.log("Mongoose server has started");
+// mongoose.connect(process.env.MONGODB_CONNECT_URL).then(() => {
+//   console.log("Mongoose server has started from atlas");
 // });
+
+mongoose.connect("mongodb://0.0.0.0:27017/usersDB").then(() => {
+  console.log("Mongoose server has started from compass");
+});
